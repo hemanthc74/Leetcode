0f1b5626c3a39fe68def5ed012c96cca905a8754
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-10 23:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.48%) - LeetSync

--- 461-hamming-distance/hamming-distance.cpp
+++ 461-hamming-distance/hamming-distance.cpp
@@ -4,8 +4,8 @@ public:
         int xorxy = x^y;
         int distance =0;
         while(xorxy>0){
-            if(xorxy & 1 == 1 ) distance++;
-            xorxy= xorxy>>1;
+            distance++;
+            xorxy&=(xorxy-1);
         }
 
         return distance;
