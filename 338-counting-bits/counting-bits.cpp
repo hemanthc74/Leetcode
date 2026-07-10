2b2a631da002d6c608db29b8661976d79f87b03c
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-10 21:38:08 +0530

    Time: 2 ms (33.47%) | Memory: 10.4 MB (97.52%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> arr(n+1) ;
+        for(int i=0 ; i<=n ;i++){
+            int cnt=0;
+            int k =i;
+            while(k>0){
+                if(k&1==1) cnt++;
+                k=k/2;
+            }
+            arr[i] = cnt;
+        }
+
+        return arr;
+    }
+};
