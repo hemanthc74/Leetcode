2b5e43f1a6a74a8df2824997955800e9d6d0bb90
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-11 18:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (57.65%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.cpp
+++ 4194-count-monobit-integers/count-monobit-integers.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int countMonobit(int n) {
-        int cnt=n+1;
-        for(int i=2;i<=n;i++){
-            int k=i;
-            while(k>1){
-                if((k&1)!=((k>>1)&1)) {
-                    cnt--;
-                    break;
-                }
-                k=k>>1;
-            }
+        int cnt=1;
+        long long current =1;
+        while(current<=n){
+            cnt++;
+            current=(current<<1)|1;
         }
         return cnt;
     }
