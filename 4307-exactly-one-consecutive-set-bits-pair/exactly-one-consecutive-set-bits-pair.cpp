e63811b154abc634b4e398ed02db4371b2d4822b
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-11 17:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (84.92%) - LeetSync

--- 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.cpp
+++ 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool consecutiveSetBits(int n) {
-        int cnt=0;
-        while(n>0){
-            if( ((n&1)==1) && ((n>>1)&1)==1 ) {
-                cnt++;
-            }
-            n=n>>1;
-        }
-        if(cnt==1) return true;
-        else return false;
+        int x = n & (n>>1);
+
+        return (x>0) && (x&(x-1))==0;
         
     }
 };
