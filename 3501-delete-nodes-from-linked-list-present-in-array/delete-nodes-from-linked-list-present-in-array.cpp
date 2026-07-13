d239ef5fabf00271f71a760cf3a3e93506276268
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-13 14:30:23 +0530

    Time: 5 ms (98.75%) | Memory: 229.3 MB (96.41%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        
+        int maxVal = 0;
+        for (int num : nums) {
+            maxVal = max(maxVal, num);
+        }
+
+        
+        vector<bool> toDelete(maxVal + 1, false);
+        for (int num : nums) {
+            toDelete[num] = true;
+        }
+
+        
         ListNode dummy(0);
         dummy.next = head;
         ListNode* temp = &dummy;
 
-        unordered_set<int> hash;
-        for(int i = 0; i < nums.size(); i++){
-            hash.insert(nums[i]); 
-        }
-
-        while(temp->next != nullptr){
-            if(hash.count(temp->next->val) > 0){
+        while (temp->next != nullptr) {
+            if (temp->next->val <= maxVal && toDelete[temp->next->val]) {
                 temp->next = temp->next->next;
             } else {
                 temp = temp->next;
