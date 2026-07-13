89e834ee61ba29d451cc77e10ce28473750fe8e5
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-13 21:26:19 +0530

    Time: 4 ms (45.55%) | Memory: 8.8 MB (74.86%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0) return false;
-        if( (n/3 == 1 && n%3 ==0) || n==1) return true;
+        if( n==1) return true;
         if(n%3!=0) return false;
         
         return isPowerOfThree(n/3);
