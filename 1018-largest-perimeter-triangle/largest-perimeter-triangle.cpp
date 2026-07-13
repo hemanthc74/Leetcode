ccf2a78375c573a8dbf75dded09327f38591ef31
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-13 22:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (14.78%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         if(n<=2) return 0;
         sort(nums.begin() , nums.end());
-        if(nums[n-1]<nums[n-2]+nums[n-3]) return nums[n-1]+nums[n-2]+nums[n-3];
+        
         for(int i=n-1 ; i>1 ;i--){
             if(nums[i]<nums[i-1]+nums[i-2]) return nums[i]+nums[i-1]+nums[i-2];
         }
