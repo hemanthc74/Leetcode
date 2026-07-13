0c664f4f3cb63c6aeab34dbaa3a178f89b3d12fb
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-13 20:22:33 +0530

    Time: 2 ms (50.06%) | Memory: 312.1 MB (53.87%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -19,9 +19,8 @@ public:
             slow=slow->next;
             fast=fast->next->next;
         }
-        ListNode * toDelete = slow->next;
+        
         slow->next = slow->next->next;
-        delete toDelete;
 
         return head;
 
