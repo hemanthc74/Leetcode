2609163e16b5e4ebe78781bfc5daee3b6eee0cec
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-13 22:38:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (98.49%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -1,25 +1,23 @@
-
 class Solution {
 public:
     int largestSumAfterKNegations(std::vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         
+        int mini = INT_MAX;
         
-        for (int i = 0; i < nums.size() && k > 0; i++) {
-            if (nums[i] < 0) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 0 && k > 0) {
                 nums[i] = -nums[i];
                 k--; 
             }
+            
+            mini = min(mini, nums[i]);
         }
-        
         
         int totalSum = accumulate(nums.begin(), nums.end(), 0);
         
-        // Step 4: If k is still odd, flip the smallest absolute element
         if (k % 2 != 0) {
-            int minElement = *min_element(nums.begin(), nums.end());
-            
-            totalSum -= 2 * minElement;
+            totalSum -= 2 * mini;
         }
         
         return totalSum;
