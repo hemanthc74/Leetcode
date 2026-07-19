c2e1a2b0bee693a6586b82f6f3fe3eef4d7139d0
hemanthc74 <hemanthroyalc74@gmail.com>
2026-07-20 00:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (75.52%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,57 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp1 = l1 ;
-        ListNode* temp2 = l2 ;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
         ListNode* dummy = new ListNode(0);
         ListNode* curr = dummy;
-        int carry =0;
-        while(temp1!=nullptr && temp2!= nullptr){
-            int sum = temp1->val + temp2->val+carry;
-            curr->next = new ListNode(sum%10);
-            carry = sum/10 ;
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-            curr = curr->next ;
-
-        }
-
-        if(temp1!=nullptr){
-            while(temp1!=nullptr){
-                int sum = temp1->val+carry ;
-                curr->next = new ListNode(sum%10);
-                carry = sum/10;
-                temp1 = temp1->next ;
-                curr = curr->next;
+        int carry = 0;
+        
+        // One loop handles both lists and the final carry
+        while (temp1 != nullptr || temp2 != nullptr || carry != 0) {
+            int sum = carry;
+            
+            if (temp1 != nullptr) {
+                sum += temp1->val;
+                temp1 = temp1->next;
             }
-        }
-
-        if(temp2!=nullptr){
-            while(temp2!=nullptr){
-                int sum = temp2->val+carry ;
-                curr->next = new ListNode(sum%10);
-                carry = sum/10;
-                temp2 = temp2->next ;
-                curr = curr->next;
+            if (temp2 != nullptr) {
+                sum += temp2->val;
+                temp2 = temp2->next;
             }
+            
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
         }
-
-        if(carry!=0){
-            curr->next = new ListNode(carry);
-            curr=curr->next;
-            curr=nullptr;
-        }
-
+        
         return dummy->next;
     }
 };
