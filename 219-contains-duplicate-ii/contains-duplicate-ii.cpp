361ca9a87809c159bc6a9c96c84b083b11db3c66
hemanthc74 <hemanthroyalc74@gmail.com>
2026-08-17 16:00:51 +0530

    Time: 134 ms (11.94%) | Memory: 134.1 MB (5.61%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,3 +1,8 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
