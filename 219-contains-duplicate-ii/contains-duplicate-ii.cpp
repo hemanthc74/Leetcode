abfc2945387ff717c614cd2081512e2baa596038
hemanthc74 <hemanthroyalc74@gmail.com>
2026-08-17 15:58:55 +0530

    Time: 139 ms (11.69%) | Memory: 134 MB (5.61%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_set<int> window;
+        for(int i=0;i<n;i++){
+            if(i>k){
+                window.erase(nums[i-k-1]);
+            }
+            if(window.count(nums[i])) return true;
+            window.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
