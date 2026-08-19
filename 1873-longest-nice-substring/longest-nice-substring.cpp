764c44f1abbe5690042f0d7da47a58df0067e95f
hemanthc74 <hemanthroyalc74@gmail.com>
2026-08-19 18:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.00%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.cpp
+++ 1873-longest-nice-substring/longest-nice-substring.cpp
@@ -1,24 +1,78 @@
 class Solution {
 public:
     string longestNiceSubstring(string s) {
-        
-        int n=s.size();
-        if(n<2) return {};
-        vector<int> hash(128,0);
-        for(auto it : s){
-            hash[it]=1;
-        }
-        
-        for(int i=0; i<n; i++){
-            char c = s[i];
-            if(hash[tolower(c)]==1 && hash[toupper(c)]==1) continue;
-            string left = longestNiceSubstring(s.substr(0,i));
-            string right = longestNiceSubstring(s.substr(i+1));
-            if(left.size()>=right.size()) return left;
-            else return right;
-        }
-        
+        int n = s.size();
 
-        return s;
+        int bestStart = 0, bestLen = 0;
+
+        for (int k = 1; k <= 26; k++) {
+
+            int lower[26] = {};
+            int upper[26] = {};
+
+            int distinct = 0;
+            int complete = 0;
+
+            int left = 0;
+
+            for (int right = 0; right < n; right++) {
+
+                int idx = tolower(s[right]) - 'a';
+
+                if (lower[idx] == 0 && upper[idx] == 0)
+                    distinct++;
+
+                if (islower(s[right])) {
+                    lower[idx]++;
+
+                    if (lower[idx] == 1 && upper[idx] > 0)
+                        complete++;
+                } else {
+                    upper[idx]++;
+
+                    if (upper[idx] == 1 && lower[idx] > 0)
+                        complete++;
+                }
+
+                while (distinct > k) {
+
+                    int removeIdx = tolower(s[left]) - 'a';
+
+                    if (islower(s[left])) {
+                        lower[removeIdx]--;
+
+                        if (lower[removeIdx] == 0 &&
+                            upper[removeIdx] > 0)
+                            complete--;
+                    } else {
+                        upper[removeIdx]--;
+
+                        if (upper[removeIdx] == 0 &&
+                            lower[removeIdx] > 0)
+                            complete--;
+                    }
+
+                    if (lower[removeIdx] == 0 &&
+                        upper[removeIdx] == 0)
+                        distinct--;
+
+                    left++;
+                }
+
+                if (distinct == k && complete == k) {
+
+                    int len = right - left + 1;
+
+                    if (len > bestLen ||
+                        (len == bestLen && left < bestStart)) {
+
+                        bestLen = len;
+                        bestStart = left;
+                    }
+                }
+            }
+        }
+
+        return s.substr(bestStart, bestLen);
     }
 };
