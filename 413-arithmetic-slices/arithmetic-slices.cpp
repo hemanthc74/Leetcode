3cbb9d1ac1f442503242fa03e8bd58d99a9f1fa3
hemanthc74 <hemanthroyalc74@gmail.com>
2026-08-20 18:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (60.16%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n=nums.size();
-        if(n<3) return 0;
-        int cnt=0;
-        for(int i=0;i<n-2;i++){
-            if(nums[i+1]-nums[i]==nums[i+2]-nums[i+1]){
-                cnt++;
-                int diff=nums[i+1]-nums[i];
-                int r = i+3;
-                while(r<n){
-                    if(nums[r]-nums[r-1]==diff) {
-                        cnt+=r-i-1;
-                        r++;
-                    }else{
-                        i=r-2;
-                        break;
-                    }
-                    if(r==n) {
-                        i=r-2;
-                    }
-                }
+        int n = nums.size();
+        if (n < 3) return 0;
+        
+        int total_cnt = 0;
+        int current_streak = 0;
+        
+        // Start from index 2 and check the difference of consecutive triplets
+        for (int i = 2; i < n; i++) {
+            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                // If the triplet forms an arithmetic progression, 
+                // it extends all previous valid slices ending at i-1 by one element,
+                // plus creates 1 new triplet slice.
+                current_streak++;
+                total_cnt += current_streak;
+            } else {
+                // If the sequence breaks, reset the streak
+                current_streak = 0;
             }
         }
-
-        return cnt;
+        
+        return total_cnt;
     }
 };
