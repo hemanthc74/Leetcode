6340dcda6fe55dabb83dab8ffcba4849c709aa99
hemanthc74 <hemanthroyalc74@gmail.com>
2026-08-21 12:21:12 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (98.63%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,17 +1,31 @@
 class Solution {
+private:
+    // Helper function to find number of subarrays with sum <= goal
+    int atMost(vector<int>& nums, int goal) {
+        if (goal < 0) return 0;
+        
+        int left = 0;
+        int current_sum = 0;
+        int count = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            current_sum += nums[right];
+            
+            // Shrink window if sum exceeds goal
+            while (current_sum > goal) {
+                current_sum -= nums[left];
+                left++;
+            }
+            
+            // Number of valid subarrays ending at 'right' index
+            count += (right - left + 1);
+        }
+        
+        return count;
+    }
+
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int> prefix_sum;
-        prefix_sum[0]=1;
-        int current_sum=0 , total_subarrays=0;
-        for(auto num:nums){
-            current_sum+=num;
-            if(prefix_sum.count(current_sum - goal)){
-                total_subarrays+=prefix_sum[current_sum - goal];
-            }
-            prefix_sum[current_sum]++;
-        }
-
-        return total_subarrays;
+        return atMost(nums, goal) - atMost(nums, goal - 1);
     }
 };
