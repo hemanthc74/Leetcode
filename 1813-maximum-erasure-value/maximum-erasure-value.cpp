76d59203c8cb68988f6913172c1a887dbc8954ce
hemanthc74 <hemanthroyalc74@gmail.com>
2026-08-24 18:58:19 +0530

    Time: 194 ms (35.34%) | Memory: 131 MB (39.88%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         unordered_set<int> hash;
-        int n=nums.size(),left=0,right=0,sum=0,maxsum=0;
-        while(right<n){
-            
-            while(hash.count(nums[right])) {
-                hash.erase(nums[left]);
-                sum-=nums[left];
-                left++;
+        int n = nums.size(), left = 0, sum = 0, maxsum = 0;
 
+        for (int right = 0; right < n; right++) {
+            while (hash.count(nums[right])) {
+                hash.erase(nums[left]);
+                sum -= nums[left];
+                left++;
             }
             hash.insert(nums[right]);
-            sum +=nums[right];
-            maxsum=max(sum,maxsum);
-            right++;
+            sum += nums[right];
+            maxsum = max(maxsum, sum);
         }
 
         return maxsum;
